3aeb95b71e2fcea6f34bead2bf572c44f61dbd2d
AHMAD FAHRIZAL <155603819+Tomketstore@users.noreply.github.com>
2024-04-04 16:24:03 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -9,13 +9,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/Tomketstore/bot/bot.zip
+wget https://raw.githubusercontent.com/Tomketstore/bot/main/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/Tomketstore/bot/addipvps.zip
+wget https://raw.githubusercontent.com/Tomketstore/bot/main/addipvps.zip
 unzip addipvps.zip
 pip3 install -r addipvps/requirements.txt
 
@@ -30,21 +30,21 @@ echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -e -p "[*] Input your Bot Token : " bottoken
 read -e -p "[*] Input Your Id Telegram : " admin
-echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
-echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
-echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
-echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
-echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
+echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/addipvps/var.txt
+echo -e ADMIN='"'$admin'"' >> /usr/bin/addipvps/var.txt
+echo -e DOMAIN='"'$domain'"' >> /usr/bin/addipvps/var.txt
+echo -e PUB='"'$PUB'"' >> /usr/bin/addipvps/var.txt
+echo -e HOST='"'$NS'"' >> /usr/bin/addipvps/var.txt
 clear
 
-cat > /etc/systemd/system/kyt.service << END
+cat > /etc/systemd/system/addipvps.service << END
 [Unit]
-Description=Simple kyt - @kyt
+Description=Simple addipvps - @kyt
 After=network.target
 
 [Service]
 WorkingDirectory=/usr/bin
-ExecStart=/usr/bin/python3 -m kyt
+ExecStart=/usr/bin/python3 -m addipvps
 Restart=always
 
 [Install]
